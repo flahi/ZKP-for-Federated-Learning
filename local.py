0201a4b0aef70276c08e9424c56d1d45f3964dd3
Faheema Kattakath Sanil <faheemakattakathsanil@gmail.com>
2025-03-12 20:09:03 +0530

    local model initial training added

---
+++ local.py
@@ -0,0 +1,156 @@
+import socket
+import threading
+import numpy as np
+import time
+import pandas as pd
+import os
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import classification_report
+from imblearn.over_sampling import SMOTE
+from sklearn.metrics import confusion_matrix
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import StandardScaler
+
+class local:
+	def __init__(self, id, port, other_ports):
+		self.id = id
+		self.port = port
+		self.other_ports = other_ports
+		self.model = RandomForestClassifier(
+			random_state = id,
+			n_estimators = 100,
+			max_depth = 12,
+			class_weight = 'balanced',
+			oob_score = True
+		)
+		threading.Thread(target=self.listen_for_data, daemon=True).start()
+	def listen_for_data(self):
+		server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		server_socket.bind(('localhost', self.port))
+		server_socket.listen()
+		print(f"Local hospital {self.id} listening on port {self.port}")
+		while True:
+			client_socket, _ = server_socket.accept()
+			threading.Thread(target=self.handle_data, args=(client_socket,), daemon=True).start()
+	def handle_data(self, client_socket):
+		data_from_client = client_socket.recv(8192).decode()
+		data = json.loads(data_from_client)
+		client_socket.close()
+	def train(self, x_train, x_test, y_train, y_test):
+		self.model.fit(x_train, y_train)
+		y_pred_proba = self.model.predict_proba(x_test)[:, 1]
+		threshold = 0.75
+		y_pred = (y_pred_proba >= threshold).astype(int)
+		accuracy = accuracy_score(y_test, y_pred)
+		print(f"Accuracy for Hospital {self.id}: {accuracy:.2f}")
+		print(classification_report(y_test, y_pred))
+		cm = confusion_matrix(y_test, y_pred)
+		print(f"Confusion Matrix for Hospital {self.id}:\n{cm}")
+
+def load_data(path):
+	if not os.path.exists(path):
+		print(f"Error: File not found at {path}")
+		return None, None
+	dataset = pd.read_csv(path)
+	print("File loaded successfully")
+	print(dataset.head())
+	X = dataset.iloc[:, :-1].values
+	Y = dataset.iloc[:, -1].values
+	return X, Y
+
+def preprocess_data(X, Y):
+	print("\nData preprocessing...")
+	imputer=SimpleImputer(missing_values=np.nan,strategy='mean')
+	imputer.fit(X[:, 1:4])
+	X[:, 1:4] = imputer.transform(X[:, 1:4])
+	imputer.fit(X[:, 5:])
+	X[:, 5:] = imputer.transform(X[:, 5:])
+	print("Missing data handled.")
+	
+	print("Unique values before change: ",np.unique(X[:,4]))
+	for i in range(X.shape[0]):
+		X[i, 4] = categorize_smoking(X[i, 4])
+	print("Unique values in the 'smoking_history' column after categorization:", np.unique(X[:, 4]))
+	print("Regularized contents of smoking history.")
+	
+	gender_mapping = {'Female': 0, 'Male': 1, 'Other': 2}
+	smoking_mapping = {'non-smoker': 0, 'current-smoker': 1, 'past-smoker': 2}
+	for i in range(X.shape[0]):
+		X[i, 0] = gender_mapping[X[i, 0]]
+	for i in range(X.shape[0]):
+		X[i, 4] = smoking_mapping[X[i, 4]]
+	X = X.astype(float)
+	print("Encoded string contents.")
+	
+	print("Data preprocessed successfully.")
+	#print("\nPreprocessed X")
+	#print(X)
+	#print("\nPreprocessed Y")
+	#print(Y)
+	return X, Y
+
+def split_data(X, Y, n):
+	print(f"\nSplitting data into {n}")
+	x_array = np.array_split(X, n)
+	y_array = np.array_split(Y, n)
+	#for i in range(n):
+	#	print(f"Hospital {i+1} - X shape: {x_array[i].shape}, Y shape: {y_array[i].shape}")
+	#	print(x_array[i])
+	return x_array, y_array
+
+def test_train(x_array, y_array, n):
+	x_train, x_test, y_train, y_test = [0]*n, [0]*n, [0]*n, [0]*n
+	for i in range(n):
+		x_train[i], x_test[i], y_train[i], y_test[i] = train_test_split(x_array[i], y_array[i],test_size=0.2,random_state=1)
+	print("Data split for training and testing.")
+	return x_train, x_test, y_train, y_test
+
+def scale_and_synthesize(x_train, x_test, y_train):
+	columns_to_scale = [1, 5, 6, 7]
+	scaler = StandardScaler()
+	x_train[:, columns_to_scale] = scaler.fit_transform(x_train[:, columns_to_scale])
+	x_test[:, columns_to_scale] = scaler.transform(x_test[:, columns_to_scale])
+	smote = SMOTE(random_state=9)
+	x_train, y_train = smote.fit_resample(x_train, y_train)
+	return x_train, x_test, y_train
+
+def create_hospitals(num, base_port=5000):
+	hospitals = []
+	ports = [base_port+i+1 for i in range(num)]
+	for i in range(num):
+		other = ports[:i] + ports[i+1:]
+		hospital = local(i+1, ports[i], other)
+		hospitals.append(hospital)
+	return hospitals
+
+def train_local_hospitals(local_hospitals, x_train, x_test, y_train, y_test, n):
+	print()
+	for i in range(n):
+		x_train[i], x_test[i], y_train[i] = scale_and_synthesize(x_train[i], x_test[i], y_train[i])
+		print(f"\nTraining local model {i+1}")
+		local_hospitals[i].train(x_train[i], x_test[i], y_train[i], y_test[i])
+
+def categorize_smoking(smoking_status):
+	if smoking_status in ['No Info', 'never']:
+		return 'non-smoker'
+	if smoking_status in ['current']:
+		return 'current-smoker'
+	if smoking_status in ['ever', 'not current', 'former']:
+		return 'past-smoker'
+
+n = 5
+local_hospitals = create_hospitals(n, 6000)
+time.sleep(0.5)
+
+path = "Sample Data/diabetes_prediction_dataset.csv"
+X, Y = load_data(path)
+
+X, Y = preprocess_data(X, Y)
+
+x_array, y_array = split_data(X, Y, n)
+
+x_train, x_test, y_train, y_test = test_train(x_array, y_array, n)
+
+train_local_hospitals(local_hospitals, x_train, x_test, y_train, y_test, n)
